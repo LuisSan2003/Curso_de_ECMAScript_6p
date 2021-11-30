b13b0fa3b887025e35cdba1523a17cbdecfb3b23
Luis Santamaria <luisignaciosantamariap@gmail.com>
2021-11-30 16:11:19 -0500

    Clase 3

--- src/es6/index.js
+++ src/es6/index.js
@@ -23,3 +23,55 @@ console.log(epicPhrase);
 // es6
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+//---------
+
+let lorem = "Sé fuerte, sé competitivo. Tu éxito depende de eso.\n"
+    + "Hazlo, por ti, por tus sueños, por la humanidad.";
+
+// es6
+
+let lorem2 = `Frase epica
+Ahora es otra frase epica`;
+console.log(lorem);
+console.log(lorem2);
+
+//-------------Desestructuracion de elementos
+
+let person = {
+    'name': 'oscar',
+    'age': 32,
+    'country': 'MX'
+}
+console.log(person.name, person.age);
+//es6
+let { name, age, country } = person;
+console.log(name, age, country);
+let { name } = person;
+console.log(name);
+
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+//es6
+let education = ['David', ...team1, ...team2];
+
+console.log(education);
+
+//------------- LET Y CONST
+var hola = "Hola"; //Estaba disponible de forma global
+// let solo esta disponible dentro del scope del bloque de codigo en el que fue asignado
+// const es un valor constante, no se podra modificar
+
+{
+    var globalVar = "Globar var";
+}
+
+{
+    let globalLet = "Global let";
+    console.log(`Dentro del bloque del let ${globalLet}`);
+}
+
+console.log(globalVar);
+
+const a = 'b';
