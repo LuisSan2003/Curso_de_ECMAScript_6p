c3916bf7b0026119cbd245d3a8aacb3c2095ca17
Luis Santamaria <luisignaciosantamariap@gmail.com>
2021-11-30 17:07:29 -0500

    Clase 6 - ECMAScript 7

---
+++ src/es7/index.js
@@ -0,0 +1,18 @@
+            //es7
+
+
+
+// includes me permite validar si existe un valor dentro de un objeto
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+if (numbers.includes(7)) {
+    console.log("Si se encuentra el valor 7");
+} else {
+    console.log("No se encuentra");
+}
+
+
+//Elevar a la potencia
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
